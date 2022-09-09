637505417e20ea357b563330c4c4aa8e729c167d
HassanMunene <awanzihassan@gmail.com>
2022-09-09 08:26:01 -0700

    alphabet game commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Description: 'The program's description'
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	return (0);
+}
