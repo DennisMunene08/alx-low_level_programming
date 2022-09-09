2e356f2f759942d686c8d21fb3d0fb21a32d211f
HassanMunene <awanzihassan@gmail.com>
2022-09-09 13:46:07 -0700

    number 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Declaration: 'The program's description'
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		putchar(',');
+		putchar(' ');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
