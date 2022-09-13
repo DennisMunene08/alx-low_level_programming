e1ca9bda262fdf2727dd785c35ae42b0795d9b57
HassanMunene <awanzihassan@gmail.com>
2022-09-13 08:01:45 -0700

    commit of task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Declaration: 'The program's description'
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+}
