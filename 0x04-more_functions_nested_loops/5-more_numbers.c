0a347cd99a5b2052ebf7d13b823c521944fabb0c
HassanMunene <awanzihassan@gmail.com>
2022-09-15 20:32:03 +0000

    my commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,12 +12,16 @@ void more_numbers(void)
 	int a;
 	int b;
 
-	for (a = 1; a <=10; a++)
+	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 14 ; b++)
 		{
-			putchar((b / 10) + '0');
-			putchar((b % 10) + '0');
+			if (b >= 0 && b <= 9)
+			{
+				putchar(b + '0');
+			}
+			putchar(b / 10 + '0');
+			putchar(b % 10 + '0');
 		}
 		putchar('\n');
 	}
