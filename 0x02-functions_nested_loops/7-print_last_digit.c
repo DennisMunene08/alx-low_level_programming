291ce41306953321ad32d5fe46d1199ea2a1769b
HassanMunene <awanzihassan@gmail.com>
2022-09-15 04:11:29 -0700

    My task 7 commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,8 @@
 #include "main.h"
 #include<stdio.h>
-
 /**
- * print_last_digit - printint the last digit in numerals
- * @a: where to put the digits
+ * print_last_digit - printint the last digit in numerals @a: where to put
+ * the digits
  *
  * Return: Always an integer
  */
@@ -12,19 +11,11 @@ int print_last_digit(int a)
 {
 	int b;
 
-	if (a >= 0)
+	b = a % 10;
+	if (b < 0)
 	{
-		b = a % 10;
-		putchar(b + '0');
-		return (b);
-		putchar('\n');
+		b = b * -1;
 	}
-	else if (a < 0)
-	{
-		b = -1 * (a % 10);
-		putchar(b + '0');
-		return (b);
-	}
-	putchar('\n');
-	return (0);
+	putchar(b + '0');
+	return (b);
 }
