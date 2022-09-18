0ecd37828c6f834a96bf1a19d8cb24b7cf8bb619
HassanMunene <awanzihassan@gmail.com>
2022-09-18 11:27:18 +0000

    correcting infinite loop

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -9,22 +9,22 @@
  * Return: Always 0 (success)
  */
 
-void positive_or_negative(int i)
+int main(void)
 {
-	int i;
+	int n;
 
 	srand(time(0));
-	i = rand() - RAND_MAX / 2;
-	if (i > 0)
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
 	{
-		printf("%d is positive\n", i);
+		printf("%d is positive\n", n);
 	}
-	else if (i == 0)
+	else if (n == 0)
 	{
-		printf("%d is zero\n", i);
+		printf("%d is zero\n", n);
 	}
 	else
-		printf("%d is negative\n", i);
+		printf("%d is negative\n", n);
 
 	return (0);
 }
