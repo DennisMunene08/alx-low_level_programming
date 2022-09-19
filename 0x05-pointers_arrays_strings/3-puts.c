60a972309b98d9b4cfa344bcc50d9f6a957d9ce2
HassanMunene <awanzihassan@gmail.com>
2022-09-19 13:17:31 +0000

    displaying string to the stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - will be used to output string to stdout
+ * @str: to hold the string
+ *
+ * Return: void 
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
