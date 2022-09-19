5df8adec3ec64b444b7d6a8c1772e95fe921749a
HassanMunene <awanzihassan@gmail.com>
2022-09-19 21:50:38 +0000

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,5 +9,31 @@
  */
 void puts_half(char *str)
 {
+	int length;
+	int n;
 
+	length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	if (length % 2 == 0)
+	{
+		n = length / 2;
+		while (length > n)
+		{
+			putchar(str[n]);
+			n++;
+		}
+	}
+	else
+	{
+		n = (length + 1) / 2;
+		while (length > n)
+		{
+			putchar(str[n]);
+			n++;
+		}
+	}
+	putchar('\n');
 }
