03dd7023d1f33f5d111fd26791fd0b997e02421c
HassanMunene <awanzihassan@gmail.com>
2022-09-26 10:40:23 -0700

    task 7

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset: fill the memory
+ * @b: the constant value to be passed
+ * @s: the address of the memory area
+ * @n: the number of bytes of the memory
+ *
+ * Return: The area pointed by s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	putchar('\n');
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - return the length of a prefix subscript
+ * @s: the address for the string
+ * @accept: the string
+ *
+ * Return: number of bytes
+ */
+
+char *_strspn(char *s, char *accept)
+{
+	unsigned int l;
+	unsigned int i;
+
+	l = 0;
+
+	while (
+}
