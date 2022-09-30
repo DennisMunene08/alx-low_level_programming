439504de02938ea0d6b029fe1339581dab0f541a
HassanMunene <awanzihassan@gmail.com>
2022-09-30 13:15:10 +0000

    the arguments of the main function

---
+++ 0x0A-arg_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the number of arguments
+ * @argc: the number of arguments
+ * @argv: the argument passed
+ *
+ * Return: Always 0
+ */
+int main(int argc, __attribute((unused))char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
