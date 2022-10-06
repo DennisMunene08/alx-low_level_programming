53990d23cc94dd646d6acc7566a746d762776ae8
HassanMunene <awanzihassan@gmail.com>
2022-10-06 12:48:57 +0000

    correction of task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,8 +5,8 @@
 /**
  * malloc_checked - will allocate memory using malloc
  *    it will return a pointer to the allocated memory
- *    if it fails to  create memory, normal process termination 
- *    should occur with a status value of 98
+ *    if it fails to  create memory, normal process termination
+ *    *    should occur with a status value of 98
  *
  * @b: this is the size of the mem in bytes
  *
