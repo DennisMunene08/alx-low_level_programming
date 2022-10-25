c85af3edd7d8fcaf393f1ede9d414911edc2e13b
HassanMunene <awanzihassan@gmail.com>
2022-10-25 19:24:25 +0000

    adding a node to the end of a linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,17 +9,52 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
-	list_t *nextp;
-	list_t *headerp;
+	char *string = strdup(str);
+	list_t *node;
+	list_t *last;
 
-	new = malloc(sizeof(list_t));
-	*head = new;
-	headerp = *head;
-	nextp = new;
-	new->str = strdup(str);
-	new->len = strlen(str);
-	new->next = new;
-	return (headerp);
+	if (!string)
+	{
+		return (0);
+	}
+	node = malloc(sizeof(list_t));
+	if (!node)
+	{
+		return (0);
+	}
+	node->str = string;
+	node->len = strlen(string);
+	node->next = NULL;
 
+	last = get_last(head);
+	if (last)
+	{
+		last->next = node;
+	}
+	else
+	{
+		*head = node;
+	}
+	return (node);
+}
+
+/**
+ * get_last - get last node of linked list
+ *
+ * Return: address of the last element
+ * @head: head node
+ */
+list_t *get_last(list_t **head)
+{
+	list_t *last = *head;
+
+	if (!last)
+	{
+		return (0);
+	}
+	while (last->next)
+	{
+		last = last->next;
+	}
+	return (last);
 }
