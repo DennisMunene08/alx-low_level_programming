f55f6ab44f1635fd0f6c6da3b094235f3507e60f
HassanMunene <awanzihassan@gmail.com>
2022-11-01 17:58:17 +0000

    binary to uint

--- 0x14-bit-manipulation/0-binary_to_uint.c
+++ 0x14-bit-manipulation/0-binary_to_uint.c
@@ -2,53 +2,31 @@
 
 /**
  * binary_to_unit - converts binary number to unsignned integr
+ * @b: pointer to astring o f 0 & 1 chars
  *
  * Return: unsigned integer
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	unsigned int size = strlen(b);
-	unsigned int i = 0;
-	unsigned int exp = size - 1;
+	unsigned int num = 0, mult = 1;
+	int len;
 
-	if (!b)
+	if (*b == '\0')
 	{
 		return (0);
 	}
-	while (b[i])
+	for (len = 0; b[len]; )
 	{
-		if (b[i] == '1')
-		{
-			num = num + power(2, exp);
-		}
-		else if (b[i] != '0')
+		len++;
+	}
+	for (len -= 1; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
 		{
 			return (0);
 		}
-		exp--;
-		i++;
-	}
-	return (num);
-}
-/**
- * pow - return the power of two numbers
- * @a: the first int
- * @b: the second
- *
- * Return: unsigned int
- */
-unsigned int power(unsigned a, unsigned b)
-{
-	unsigned int i;
-	unsigned int num;
-	if (b == 0)
-	{
-		return (1);
-	}
-	for (i = 1; i <= b; i++)
-	{
-		num = num * a;
+		num += (b[len] - '0') * mult;
+		mult *= 2;
 	}
 	return (num);
 }
