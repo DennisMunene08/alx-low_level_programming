38ee48694191a0aa0d0da4d16c4b4971630e8104
HassanMunene <awanzihassan@gmail.com>
2022-11-01 20:27:02 +0000

    more corrections

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,70 +1,17 @@
 #include "main.h"
-
 /**
- * set_bit - set bit to 1 at index
- * @n: pointer to number
- * @index: the inndex
- *
- * Return: the given index
+ * *set_bit-function that sets the value of a bit to 1
+ * *@n: A ptr to a bit
+ * *@index: the index to set the value
+ * *Return: if an error occure -1 otherwise 1
  */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int copy;
-
-	if (index > 64)
+	if (index >= (sizeof(unsigned long int) * 8))
 	{
 		return (-1);
 	}
-	copy = *n;
-	copy = get_bit(copy, index);
-
-	if (copy == 0x01)
-	{
-		return (1);
-	}
-	*n = (*n) + power(2, index);
+	*n ^= (1 << index);
 	return (1);
-}
-/**
- * pow - return the power of two numbers
- * @a: the first int
- * @b: the second
- *
- * Return: unsigned int
- */
-unsigned int power(unsigned a, unsigned b)
-{
-	unsigned int i;
-	unsigned int num;
-	if (b == 0)
-	{
-		return (1);
-	}
-	for (i = 1; i <= b; i++)
-	{
-		num = num * a;
-	}
-	return (num);
-}
-
-/**
- * get_bit - get value of bit at a certain index
- * @n: value
- * @index: the given index
- *
- * Return: index given
- */
-int get_bit(unsigned long int n, unsigned int index)
-{
-	unsigned int c;
-
-	if (index > 64)
-	{
-		return (-1);
-	}
-	for (c = 0; c < index; c++)
-	{
-		n >>= 0x01;
-	}
-	return (n & 0x01);
 }
